bb1d27d8d6a1e10e57894a724247556a38038fba
Michael <80859948+bridgemami@users.noreply.github.com>
2021-11-11 17:30:15 +0000

    new spritesheet

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
      You will need to replace the contents of this JavaScript file with your own work, 
      and create any other files, if any, required for the assignment.
      When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+//step 1. define the name's characteristics in an object value saved in a variable to use later
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    // tell phaser engine how to find the blocks of code (functions) I am providing for the game
     scene: {
         preload: preload,
         create: create,
@@ -19,6 +21,7 @@ var config = {
       }
     }
 };
+// step 2. define global variables that al functions can access to work with game objects
 var player;
 var stars;
 var platforms;
@@ -27,22 +30,30 @@ var score = 0;
 var gameOver = false;
 var scoreText;
 var game = new Phaser.Game(config);
-//inserting the png
+// step 3. PRELOAD the phaser library runs our function for preload to load all the images and audio for the game
 function preload ()
-{
-  this.load.image('sky', 'assets/sky.png');
+{// load a couple of image file assets to use during create
+    // image() takes 2 args: string nickname for the asset, filepath to file
+  this.load.image('sky', 'assets/cloud.png');
+  this.load.image('tree', 'assets/tree.png');
+  this.load.image('grass', 'assets/grass.png');
   this.load.image('ground', 'assets/platform.png');
   this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png')
-  this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
+  this.load.image('bomb', 'assets/bomb.png');
+  // spritesheet() takes 3 args: string nickname for the asset, filepath to file, frame dimension
+  //this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
+  this.load.spritesheet('dude', 'assets/officer.png', {frameWidth: 64, frameHeight: 64});
   console.log("1. images done");
 }
 console.log("2. preload done");
 
-//creating the stage
+// step 4. CREATE the phaser library next runs our function for creating/laying out the game, bkgnd, create game objects, etc.
 function create ()
-{//background
-  this.add.image(400, 300, 'sky');
+{// display a game object in the visible game stage
+    // 3 args: x-coord, y-coord, asset nickname string
+  this.add.image(400, 300, 'sky').setScale(2.7);
+  this.add.image(400, 300, 'grass');
+  this.add.image(400, 300, 'tree').setScale(2.7);
   //this.add.image(0, 0, 'sky').setOrigin(0, 0);
   //the star image
   //this.add.image(400, 300, 'star');
@@ -56,25 +67,28 @@ function create ()
    platforms.create(50, 250, 'ground');
    platforms.create(750, 220, 'ground');
    console.log("4. platforms done");
-//add the avatar & sprite
+// add another game object, this time a player-controlled object often called a sprite
+    // to work with game objects in multiple stage of a scene, save a ref to obj in a global var
+    // sprite() takes 3 args: x-coord, y-coord, asset nickname string
 player = this.physics.add.sprite(100, 450, 'dude');
 //movement
 player.setBounce(0.2);
+// ask the physics sim in the engine to prevent a game object from falling off screen
 player.setCollideWorldBounds(true);
 this.anims.create ({
   key: 'left',
-  frames : this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+  frames : this.anims.generateFrameNumbers('dude', {start: 9, end: 16}), 
   frameRate: 10,
   repeat: -1
 });
 this.anims.create({
   key: 'turn',
-  frames: [{key: 'dude', frame: 4}],
+  frames: [{key: 'dude', frame: 18}],
   frameRate: 20
 });
 this.anims.create({
   key: 'right',
-  frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+  frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 7}), 
   frameRate: 10,
   repeat: -1
 });
@@ -107,12 +121,14 @@ console.log("7. keyboard");
 //}
 console.log("6. create done");
 function update ()
-{ if(gameOver) {
+{ //look at the global var that Phaser is saving the state of arrows inside
+ // check left and right arrow keys
+  if(gameOver) {
   return;
 }
   
   //cursors = this.input.keyboard.createCursorKeys();
-  //add movement
+  //check left and right arrow keys
 if (cursors.left.isDown){
   player.setVelocityX(-160);
   player.anims.play('left', true);
@@ -127,6 +143,12 @@ else if (cursors.right.isDown) {
       player.setVelocityY(-330);
       console.log("7. keyboard");
   }
+  /*if(cursors.up.isDown){
+   player.setVelocityY(-160);}
+else if (cursors.down.isDown){player.setVelocityY(160);}
+ else {
+  // could change vertical velocity but gravity would be messed up
+ }*/
 }
 function collectStar(player, star) {
     star.disableBody(true, true);
